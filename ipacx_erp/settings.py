1c0276cb471324ff1e4cc19ba80518659ed1b3bb
IPACX Admin <admin@ipacx.com>
2026-01-15 18:27:48 +0530

    Enable CORS Credentials

--- ipacx_erp/settings.py
+++ ipacx_erp/settings.py
@@ -32,6 +32,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
+CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
