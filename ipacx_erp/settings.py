642cac9764cb7e5dd1ba3892a0a83205540d07e6
IPACX Admin <admin@ipacx.com>
2026-01-15 18:23:34 +0530

    Explicitly allow localhost:5173 in CORS

--- ipacx_erp/settings.py
+++ ipacx_erp/settings.py
@@ -28,6 +28,10 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
